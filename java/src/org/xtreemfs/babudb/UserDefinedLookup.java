5d9e6f8a3837011de02553b66ee0835dbd099331
bjoern.kolbeck <bjoern.kolbeck@d4c7dd96-b7b2-11dd-893d-d98698f66bbf>
2008-12-12 14:23:44 +0000

    fixed javadocs for user defined lookup methods
    git-svn-id: https://babudb.googlecode.com/svn/trunk@28 d4c7dd96-b7b2-11dd-893d-d98698f66bbf

--- java/src/org/xtreemfs/babudb/UserDefinedLookup.java
+++ java/src/org/xtreemfs/babudb/UserDefinedLookup.java
@@ -14,6 +14,12 @@ import org.xtreemfs.babudb.lsmdb.LSMLookupInterface;
  */
 public interface UserDefinedLookup {
 
+    /**
+     * The method which is executed by the worker thread.
+     * @param database direct access to synchronous database lookups
+     * @return the result which is passed on to the listener
+     * @throws org.xtreemfs.babudb.BabuDBException in case of an error, is passed to the listener
+     */
     public Object execute(final LSMLookupInterface database) throws BabuDBException;
     
 }
