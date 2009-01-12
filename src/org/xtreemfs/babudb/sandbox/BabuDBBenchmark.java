13a36b21667aa4df3c5f48c4b50cdf58115247ef
bjoern.kolbeck <bjoern.kolbeck@d4c7dd96-b7b2-11dd-893d-d98698f66bbf>
2009-01-12 12:27:02 +0000

    enhanced benchmarking tool
    git-svn-id: https://babudb.googlecode.com/svn/trunk/java@43 d4c7dd96-b7b2-11dd-893d-d98698f66bbf

--- src/org/xtreemfs/babudb/sandbox/BabuDBBenchmark.java
+++ src/org/xtreemfs/babudb/sandbox/BabuDBBenchmark.java
@@ -1,10 +1,18 @@
 /*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
+ * Copyright (c) 2009, Jan Stender, Bjoern Kolbeck, Mikael Hoegqvist,
+ *                     Felix Hupfeld, Zuse Institute Berlin
+ *
+ * Licensed under the BSD License, see LICENSE file for details.
+ *
+*/
 
 package org.xtreemfs.babudb.sandbox;
 
+import java.io.File;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import org.xtreemfs.babudb.BabuDB;
 import org.xtreemfs.babudb.BabuDBException;
 import org.xtreemfs.babudb.log.DiskLogger.SyncMode;
@@ -18,7 +26,7 @@ public class BabuDBBenchmark {
 
     public static final String VER = "0.1";
 
-    private final static byte[] CHARS;
+    public final static byte[] CHARS;
 
     static {
         int charptr = 0;
@@ -39,10 +47,14 @@ public class BabuDBBenchmark {
 
     private final BabuDB database;
 
-    public BabuDBBenchmark(String dbDir, SyncMode syncMode, int numKeys, int valueLength, int minKeyLength, int maxKeyLength) throws BabuDBException {
+    private final int numThreads;
+
+    public BabuDBBenchmark(String dbDir, SyncMode syncMode, int pseudoModeWait, int maxQ, int numThreads, int numDBWorkers,
+            int numKeys, int valueLength, int minKeyLength, int maxKeyLength) throws BabuDBException {
         this.numKeys = numKeys;
         this.minKeyLength = minKeyLength;
         this.maxKeyLength = maxKeyLength;
+        this.numThreads = numThreads;
 
         payload = new byte[valueLength];
         for (int i = 0; i < valueLength; i++)
@@ -52,36 +64,81 @@ public class BabuDBBenchmark {
             throw new IllegalArgumentException(maxKeyLength+" is too short to create enough unique keys for "+numKeys+" keys");
 
         //use one worker because we use one database
-        database = new BabuDB(dbDir, dbDir, 1, 1, 0, syncMode, 200, 500);
-        database.createDatabase("testdb", 1);
+        database = new BabuDB(dbDir, dbDir, numDBWorkers, 1, 0, syncMode, pseudoModeWait, maxQ);
+        for (int i = 1; i <= numThreads; i++)
+        database.createDatabase(""+i, 1);
 
-        System.out.println("initialized BabuDBBenchmark version "+VER);
-        System.out.println("database directory: "+dbDir);
-        System.out.println("fsync disabled:     "+syncMode.name());
-        System.out.println("min/max key length: "+minKeyLength+"/"+maxKeyLength);
-        System.out.println("number of workers:  1");
-        System.out.println("auto checkpointing: disabled");
-        System.out.println("value size:         "+valueLength);
-    }
+        System.out.println("BabuDBBenchmark version "+VER+" ==============================\n");
+        System.out.println("Configuration ----------------------------------");
+        System.out.println("database directory:       "+dbDir);
+        System.out.println("sync mode:                "+syncMode.name());
+        System.out.println("pseudo-sync mode:         "+( (pseudoModeWait > 0) ? "enabled, "+pseudoModeWait+" ms" : "disabled"));
+        System.out.println("max. queue length:        "+( (maxQ > 0) ? maxQ+" requests" : "unlimited"));
+        System.out.println("min/max key length:       "+minKeyLength+"/"+maxKeyLength);
+        System.out.println("# benchmark threads/DBs:  "+numThreads);
+        System.out.println("# BabuDB workers:         "+numDBWorkers);
+        System.out.println("auto checkpointing:       disabled");
+        System.out.println("value size:               "+valueLength);
+        System.out.println("num keys/worker:          "+numKeys);
+        System.out.println("");
+        System.out.println("Executing benchmark...\n");
+    }   
 
-    public void performInserts() throws BabuDBException {
+    public double checkpoint() throws BabuDBException, InterruptedException {
         long tStart = System.currentTimeMillis();
-        for (int i = 0; i < numKeys; i++) {
-            byte[] key = createRandomKey();
-            database.syncSingleInsert("testdb", 0, key, payload);
-        }
-        long tEnd = System.currentTimeMillis();
-        double dur = (tEnd-tStart);
-        System.out.format("insert of %d keys took %.4f seconds\n",numKeys,dur/1000.0);
-        System.out.format("       = %.4f s/key\n",(dur/1000.0)/((double)numKeys));
-    }
-
-    public void checkpoint() throws BabuDBException, InterruptedException {
         database.checkpoint();
+        long tEnd = System.currentTimeMillis();
+        return (tEnd-tStart)/1000.0;
     }
 
     public void shutdown() {
         database.shutdown();
+    }
+
+    public double benchmarkInserts() throws Exception {
+        BenchmarkWorkerThread[] threads = new BenchmarkWorkerThread[numThreads];
+        for (int i = 0; i < numThreads; i++) {
+            threads[i] = new BenchmarkWorkerThread(i+1, numKeys, minKeyLength, maxKeyLength, payload, database, BenchmarkWorkerThread.BenchmarkOperation.INSERT);
+        }
+        for (int i = 0; i < numThreads; i++) {
+            threads[i].start();
+        }
+        double throughput = 0.0;
+        for (int i = 0; i < numThreads; i++) {
+            throughput += threads[i].waitForResult();
+        }
+        return throughput;
+        
+    }
+
+    public double benchmarkIterate() throws Exception {
+        BenchmarkWorkerThread[] threads = new BenchmarkWorkerThread[numThreads];
+        for (int i = 0; i < numThreads; i++) {
+            threads[i] = new BenchmarkWorkerThread(i+1, numKeys, minKeyLength, maxKeyLength, payload, database, BenchmarkWorkerThread.BenchmarkOperation.ITERATE);
+        }
+        for (int i = 0; i < numThreads; i++) {
+            threads[i].start();
+        }
+        double throughput = 0.0;
+        for (int i = 0; i < numThreads; i++) {
+            throughput += threads[i].waitForResult();
+        }
+        return throughput;
+    }
+
+    public double benchmarkLookup() throws Exception {
+        BenchmarkWorkerThread[] threads = new BenchmarkWorkerThread[numThreads];
+        for (int i = 0; i < numThreads; i++) {
+            threads[i] = new BenchmarkWorkerThread(i+1, numKeys, minKeyLength, maxKeyLength, payload, database, BenchmarkWorkerThread.BenchmarkOperation.LOOKUP);
+        }
+        for (int i = 0; i < numThreads; i++) {
+            threads[i].start();
+        }
+        double throughput = 0.0;
+        for (int i = 0; i < numThreads; i++) {
+            throughput += threads[i].waitForResult();
+        }
+        return throughput;
     }
 
     private byte[] createRandomKey() {
@@ -91,19 +148,81 @@ public class BabuDBBenchmark {
 
         byte[] key = new byte[length];
         for (int i = 0; i < length; i++)
-            key[i] = CHARS[(int)Math.random()*(CHARS.length-1)];
+            key[i] = CHARS[(int)(Math.random()*(CHARS.length-1))];
         return key;
     }
+
+    public static void usage() {
+        System.out.println("BabuDBBenchmark <options> <numKeysPerThread>");
+        System.out.println("  "+"<numKeysPerThread> number of keys inserted/looked-up by");
+        System.out.println("  "+"each thread");
+        System.out.println("  "+"-path directory in which to store the database, default is /tmp/babudb_benchmark");
+        System.out.println("  "+"-sync synchronization mode FSYNC");
+        System.out.println("  "+"-wait ms between to bach writes, default is 0 for synchronous mode");
+        System.out.println("  "+"-maxq maxmimum worker queue length, default is 0 for unlimited");
+        System.out.println("  "+"-workers number of database worker threads, default is 1");
+        System.out.println("  "+"-thr number of threads to use for benchmarking, default is 1");
+        System.out.println("  "+"-payload size of values, default is 50 bytes");
+        System.out.println("  "+"-keymin minimum key length, default is 2");
+        System.out.println("  "+"-keymax maximum key length, default is 20");
+    }
+
 
     public static void main(String[] args) {
         try {
             Logging.start(Logging.LEVEL_WARN);
-            BabuDBBenchmark benchmark = new BabuDBBenchmark("/tmp/babubench", SyncMode.FDATASYNC, 10000, 50, 2, 20);
-            benchmark.performInserts();
+
+            Map<String,CLIParser.CliOption> options = new HashMap();
+            options.put("path",new CLIParser.CliOption(CLIParser.CliOption.OPTIONTYPE.FILE,new File("/tmp/babudb_benchmark")));
+            options.put("sync",new CLIParser.CliOption(CLIParser.CliOption.OPTIONTYPE.STRING,"FSYNC"));
+            options.put("wait", new CLIParser.CliOption(CLIParser.CliOption.OPTIONTYPE.NUMBER,0));
+            options.put("maxq", new CLIParser.CliOption(CLIParser.CliOption.OPTIONTYPE.NUMBER,0));
+            options.put("workers", new CLIParser.CliOption(CLIParser.CliOption.OPTIONTYPE.NUMBER,1));
+            options.put("thr", new CLIParser.CliOption(CLIParser.CliOption.OPTIONTYPE.NUMBER,1));
+            options.put("payload", new CLIParser.CliOption(CLIParser.CliOption.OPTIONTYPE.NUMBER,50));
+            options.put("keymin", new CLIParser.CliOption(CLIParser.CliOption.OPTIONTYPE.NUMBER,2));
+            options.put("keymax", new CLIParser.CliOption(CLIParser.CliOption.OPTIONTYPE.NUMBER,20));
+
+
+            List<String> arguments = new ArrayList(1);
+            CLIParser.parseCLI(args, options, arguments);
+
+            if ((arguments.size() != 1) || (options.get("h").switchValue)) {
+                usage();
+                System.exit(1);
+            }
+
+            int numKeys = Integer.valueOf(arguments.get(0));
+
+
+            BabuDBBenchmark benchmark = new BabuDBBenchmark(options.get("path").fileValue.getAbsolutePath(),
+                    SyncMode.valueOf(options.get("sync").stringValue),
+                    options.get("wait").numValue.intValue(),
+                    options.get("maxq").numValue.intValue(),
+                    options.get("thr").numValue.intValue(),
+                    options.get("workers").numValue.intValue(),
+                    numKeys,
+                    options.get("payload").numValue.intValue(),
+                    options.get("keymin").numValue.intValue(),
+                    options.get("keymax").numValue.intValue());
+            double tpIns =benchmark.benchmarkInserts();
+            double tpIter = benchmark.benchmarkIterate();
+            double durCP = benchmark.checkpoint();
+            double tpLookup = benchmark.benchmarkLookup();
+
+            System.out.println("RESULTS -----------------------------------------\n");
+            
+            System.out.format("total throughput for INSERT : %12.4f keys/s\n", tpIns);
+            System.out.format("total throughput for ITERATE: %12.4f keys/s\n", tpIter);
+            System.out.format("total throughput for LOOKUP : %12.4f keys/s\n\n", tpLookup);
+
+            System.out.format("CHECKPOINTING took          : %12.4f s\n", durCP);
+
             benchmark.shutdown();
         } catch (Exception ex) {
             System.out.println("FAILED!!!");
             ex.printStackTrace();
+            System.exit(1);
         }
     }
 
---
+++ src/org/xtreemfs/babudb/sandbox/BenchmarkWorkerThread.java
@@ -0,0 +1,164 @@
+/*
+ * Copyright (c) 2009, Jan Stender, Bjoern Kolbeck, Mikael Hoegqvist,
+ *                     Felix Hupfeld, Zuse Institute Berlin
+ *
+ * Licensed under the BSD License, see LICENSE file for details.
+ *
+*/
+
+package org.xtreemfs.babudb.sandbox;
+
+import java.util.Iterator;
+import java.util.Map.Entry;
+import org.xtreemfs.babudb.BabuDB;
+import org.xtreemfs.babudb.BabuDBException;
+
+/**
+ *
+ * @author bjko
+ */
+public class BenchmarkWorkerThread extends Thread {
+
+    public static enum BenchmarkOperation {
+        INSERT,
+        ITERATE,
+        LOOKUP
+    };
+
+    private final int numKeys;
+
+    private final int id;
+
+    private final int minKeyLength;
+
+    private final int maxKeyLength;
+
+    private final byte[] payload;
+
+    private final BabuDB database;
+
+    private final BenchmarkOperation operation;
+
+    private transient boolean done;
+
+    private transient Exception error;
+
+    private transient double    throughput;
+
+    private final String dbName;
+
+    public BenchmarkWorkerThread(int id, int numKeys, int minKeyLength,
+            int maxKeyLength, byte[] payload, BabuDB database, BenchmarkOperation operation) {
+        this.id = id;
+        this.numKeys = numKeys;
+        this.minKeyLength = minKeyLength;
+        this.maxKeyLength = maxKeyLength;
+        this.payload = payload;
+        this.database = database;
+        this.operation = operation;
+        this.dbName = Integer.toString(id);
+        error = null;
+        done = false;
+    }
+
+    public void run() {
+        try {
+            switch (operation) {
+                case INSERT : performInserts(); break;
+                case LOOKUP : performLookups(); break;
+                case ITERATE : countKeys(); break;
+            }
+        } catch (Exception ex) {
+            error = ex;
+        }
+        synchronized (this) {
+            done = true;
+            this.notifyAll();
+        }
+
+    }
+
+    public double waitForResult() throws Exception {
+        synchronized (this) {
+            if (!done) {
+               this.wait();
+            }
+            if (error != null) {
+                throw error;
+            }
+            return throughput;
+        }
+    }
+
+    public void performInserts() throws BabuDBException {
+        long tStart = System.currentTimeMillis();
+        byte[] key = null;
+        for (int i = 0; i < numKeys; i++) {
+            key = createRandomKey();
+            database.syncSingleInsert(dbName, 0, key, payload);
+        }
+        long tEnd = System.currentTimeMillis();
+        double dur = (tEnd-tStart);
+
+        String output = String.format("   thread #%3d: insert of %d keys took %.4f seconds\n", id ,numKeys,dur/1000.0);
+        output += String.format("   thread #%3d:       = %10.4f s/key\n",id,(dur/1000.0)/((double)numKeys));
+        output += String.format("   thread #%3d:       = %10.4f keys/s\n\n",id,((double)numKeys)/(dur/1000.0));
+
+        throughput = ((double)numKeys)/(dur/1000.0);
+
+        System.out.print(output);
+    }
+
+    public void countKeys() throws BabuDBException {
+        long tStart = System.currentTimeMillis();
+        Iterator<Entry<byte[],byte[]>> iter = database.syncPrefixLookup(dbName, 0, new byte[]{});
+        int count = 0;
+        while (iter.hasNext()) {
+            Entry<byte[],byte[]> e = iter.next();
+            count++;
+        }
+        long tEnd = System.currentTimeMillis();
+        double dur = (tEnd-tStart);
+        String output = String.format("   thread #%3d: iterating over %d keys took %.4f seconds\n\n",id,count,dur/1000.0);
+
+        throughput = ((double)count)/(dur/1000.0);
+
+        System.out.print(output);
+    }
+
+    public void performLookups() throws BabuDBException {
+        long tStart = System.currentTimeMillis();
+        byte[] key = null;
+        int hits = 0;
+        for (int i = 0; i < numKeys; i++) {
+            key = createRandomKey();
+            byte[] value = database.syncLookup(dbName, 0, key);
+            if (value != null)
+                hits++;
+        }
+        long tEnd = System.currentTimeMillis();
+        double dur = (tEnd-tStart);
+        
+        String output = String.format("   thread #%3d: lookup of %d keys took %.4f seconds\n",id,numKeys,dur/1000.0);
+        output += String.format("   thread #%3d:       = %10.4f s/key\n",id,(dur/1000.0)/((double)numKeys));
+        output += String.format("   thread #%3d:       = %10.4f keys/s\n",id,((double)numKeys)/(dur/1000.0));
+        output += String.format("   thread #%3d: hit-rate %6.2f%%\n\n",id,(hits*100.0)/(double)numKeys);
+
+        throughput = ((double)numKeys)/(dur/1000.0);
+
+        System.out.print(output);
+    }
+
+    private byte[] createRandomKey() {
+        final int length = (int) (Math.random() * ((double)(maxKeyLength - minKeyLength)) + minKeyLength);
+        assert(length >= minKeyLength);
+        assert(length <= maxKeyLength);
+
+        byte[] key = new byte[length];
+        for (int i = 0; i < length; i++)
+            key[i] = BabuDBBenchmark.CHARS[(int)(Math.random()*(BabuDBBenchmark.CHARS.length-1))];
+        return key;
+    }
+
+
+}
