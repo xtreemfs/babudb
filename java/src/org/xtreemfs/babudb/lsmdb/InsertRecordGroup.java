122385eb0789254e420aa4d2a380b9ca312bdc5e
stenjan <stenjan@d4c7dd96-b7b2-11dd-893d-d98698f66bbf>
2009-02-04 11:45:55 +0000

    * added debugging functionality: InsertGroups now offer a 'toString()' method
    git-svn-id: https://babudb.googlecode.com/svn/trunk@58 d4c7dd96-b7b2-11dd-893d-d98698f66bbf

--- java/src/org/xtreemfs/babudb/lsmdb/InsertRecordGroup.java
+++ java/src/org/xtreemfs/babudb/lsmdb/InsertRecordGroup.java
@@ -4,23 +4,24 @@
  * 
  * Licensed under the BSD License, see LICENSE file for details.
  * 
-*/
+ */
 
 package org.xtreemfs.babudb.lsmdb;
 
+import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
 import org.xtreemfs.common.buffer.ReusableBuffer;
 
 /**
- *
+ * 
  * @author bjko
  */
 public class InsertRecordGroup {
     
     private List<InsertRecord> records;
     
-    private int databaseId;
+    private int                databaseId;
     
     public InsertRecordGroup(int databaseId) {
         records = new LinkedList();
@@ -28,7 +29,7 @@ public class InsertRecordGroup {
     }
     
     public void addInsert(int indexId, byte[] key, byte[] value) {
-        records.add(new InsertRecord(indexId,key,value));
+        records.add(new InsertRecord(indexId, key, value));
     }
     
     public List<InsertRecord> getInserts() {
@@ -40,17 +41,16 @@ public class InsertRecordGroup {
     }
     
     private void addInsertRecord(InsertRecord rec) {
-        assert(rec != null);
+        assert (rec != null);
         records.add(rec);
     }
     
     public int getSize() {
-        int size = Integer.SIZE/8;
+        int size = Integer.SIZE / 8;
         for (InsertRecord ir : records)
             size += ir.getSize();
         return size;
     }
-    
     
     public void serialize(ReusableBuffer buffer) {
         buffer.putInt(databaseId);
@@ -68,35 +68,36 @@ public class InsertRecordGroup {
         return ai;
     }
     
-
     public static class InsertRecord {
-        private final byte indexId;
+        private final byte   indexId;
+        
         private final byte[] key;
+        
         private final byte[] value;
         
         public InsertRecord(int indexId, byte[] key, byte[] value) {
-            this.indexId = (byte)indexId;
+            this.indexId = (byte) indexId;
             this.key = key;
             this.value = value;
         }
-
+        
         public int getIndexId() {
             return 0x00FF & indexId;
         }
-
+        
         public byte[] getKey() {
             return key;
         }
-
+        
         public byte[] getValue() {
             return value;
         }
         
         public int getSize() {
             if (value != null)
-                return Byte.SIZE/8+Integer.SIZE/8*2+key.length+value.length;
+                return Byte.SIZE / 8 + Integer.SIZE / 8 * 2 + key.length + value.length;
             else
-                return Byte.SIZE/8+Integer.SIZE/8*2+key.length;
+                return Byte.SIZE / 8 + Integer.SIZE / 8 * 2 + key.length;
         }
         
         public void serialize(ReusableBuffer buffer) {
@@ -110,7 +111,6 @@ public class InsertRecordGroup {
                 buffer.putInt(0);
             }
         }
-        
         
         public static InsertRecord deserialize(ReusableBuffer buffer) {
             byte tmp = buffer.get();
@@ -127,8 +127,30 @@ public class InsertRecordGroup {
                 buffer.get(value);
             }
             
-            return new InsertRecord(indexId,key,value);
+            return new InsertRecord(indexId, key, value);
         }
         
+        public String toString() {
+            
+            StringBuilder sb = new StringBuilder();
+            sb.append("index: " + indexId);
+            sb.append(", key: " + (key == null ? null : Arrays.toString(key)));
+            sb.append(", value: " + (value == null ? null : Arrays.toString(value)));
+            
+            return sb.toString();
+        }
+        
+    }
+    
+    public String toString() {
+        
+        StringBuilder sb = new StringBuilder();
+        
+        sb.append(getClass().getName() + "\n");
+        sb.append("database: " + databaseId + "\n");
+        for (InsertRecord record : records)
+            sb.append(record + "\n");
+        
+        return sb.toString();
     }
 }
