4a2e87bfd14b36e9835f50dff5b3eac7627f0eea
stenjan <stenjan@d4c7dd96-b7b2-11dd-893d-d98698f66bbf>
2010-09-20 08:44:23 +0000

    0.4.3 release: added changelog entry, increased version number
    git-svn-id: https://babudb.googlecode.com/svn/trunk@309 d4c7dd96-b7b2-11dd-893d-d98698f66bbf

--- java/src/org/xtreemfs/babudb/BabuDB.java
+++ java/src/org/xtreemfs/babudb/BabuDB.java
@@ -60,7 +60,7 @@ public class BabuDB {
     /**
      * Version (name)
      */
-    public static final String           BABUDB_VERSION           = "0.4.2";
+    public static final String           BABUDB_VERSION           = "0.4.3";
     
     /**
      * Version of the DB on-disk format (to detect incompatibilities).
