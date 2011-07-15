744ca937df9768e8e456fc1adf9c1dc456ea6a98
stenjan <stenjan@d4c7dd96-b7b2-11dd-893d-d98698f66bbf>
2011-07-15 09:11:56 +0000

    BabuDB/Java: updated change logs and version number for 0.5.3 release
    git-svn-id: https://babudb.googlecode.com/svn/trunk@528 d4c7dd96-b7b2-11dd-893d-d98698f66bbf

--- java/src/org/xtreemfs/babudb/BabuDBFactory.java
+++ java/src/org/xtreemfs/babudb/BabuDBFactory.java
@@ -32,7 +32,7 @@ public final class BabuDBFactory {
     /**
      * Version (name)
      */
-    public static final String BABUDB_VERSION           = "0.5.2";
+    public static final String BABUDB_VERSION           = "0.5.3";
     
     /**
      * Version of the DB on-disk format (to detect incompatibilities).
