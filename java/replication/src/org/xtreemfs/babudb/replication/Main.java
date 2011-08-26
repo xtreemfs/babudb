aaf47ce3d6a0467f8291d544cbcf9e008edfa326
stenjan <stenjan@d4c7dd96-b7b2-11dd-893d-d98698f66bbf>
2011-08-26 14:27:44 +0000

    BabuDB/Java: updated change logs and version number for 0.5.4 release
    git-svn-id: https://babudb.googlecode.com/svn/trunk@581 d4c7dd96-b7b2-11dd-893d-d98698f66bbf

--- java/replication/src/org/xtreemfs/babudb/replication/Main.java
+++ java/replication/src/org/xtreemfs/babudb/replication/Main.java
@@ -74,7 +74,7 @@ public class Main extends PluginMain {
      */
     @Override
     public String compatibleBabuDBVersion() {
-        return PluginMain.buildCompatibleVersionString(0, 5, 0, 0, 5, 9);
+        return PluginMain.buildCompatibleVersionString(0, 5, 4, 0, 5, 9);
     }
 
     /* (non-Javadoc)
